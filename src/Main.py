593273c7ddd1654a8d9688317b3a5ee5207122e9
Brian Crane <briancrane@gmail.com>
2022-01-23 15:13:42 -0800

    Adding Tools class to help Main

--- src/Main.py
+++ src/Main.py
@@ -1,12 +1,19 @@
-from selenium import webdriver
-from selenium.webdriver.chrome.options import Options
-from selenium.webdriver.common.by import By
+import time
 
-options = Options()
-options.add_argument("user-data-dir=C:/Users/brian/AppData/Local/Google/Chrome/User Data - Copy")
-driver = webdriver.Chrome(executable_path='C:/Users/brian/Downloads/chromedriver.exe', chrome_options=options)
+from selenium.webdriver import Keys
 
-driver.get("https://www.indeed.com/")
-driver.find_element(By.ID, "text-input-what").click()
-driver.find_element(By.ID, "text-input-what").send_keys("test")
-driver.close()
+from Tools import Tools
+
+myTools = Tools()
+myTools.go_to("indeed.com")
+
+searchBar = myTools.get_element_by_id("text-input-what")
+searchBar.click()
+searchBar.send_keys("QA Engineer")
+searchBar.send_keys(Keys.ENTER)
+
+time.sleep(5)
+
+myTools.kill()
+
+print("Goodbye")
--- src/Tools.py
+++ src/Tools.py
@@ -1,4 +1,28 @@
+import time
 
-# Gets a list of Links to all Job Postings matching the searchTerm
-def getJobPostings(driver, searchTerm):
-    return 0
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.wait import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+
+options = Options()
+options.add_argument("user-data-dir=C:/Users/brian/AppData/Local/Google/Chrome/User Data - Copy")
+driver = webdriver.Chrome(executable_path='C:/Users/brian/Downloads/chromedriver.exe', chrome_options=options)
+time.sleep(1)
+
+class Tools:
+
+    def go_to(self, url):
+        if "http" not in url:
+            url = "https://" + url
+            driver.get(url)
+
+    def get_job_postings(self, searchTerm):
+        return 0
+
+    def get_element_by_id(self, aid):
+        return WebDriverWait(driver, 10).until(EC.presence_of_element_located((By.ID, aid)))
+
+    def kill(self):
+        driver.close()
